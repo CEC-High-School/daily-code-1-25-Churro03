f5f2ba5e9ee53fa394a425bec325928f37f8728c
Churro03 <35495503+Churro03@users.noreply.github.com>
2018-01-25 08:11:25 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,23 @@
+#include<iostream>
+using namespace std;
+
+char letter1;
+char letter2;
+char letter3;
 
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+
+int main() {
+	
+	cout << "Enter the 3 letter password, one at a time please and thank you." << endl;
+	cin >> letter1;
+	cin >> letter2;
+	cin >> letter3;
+
+	if (letter1 == 'N' && letter2 == 'E' && letter3 == 'S')
+		cout << "welcome to Jurassic World" << endl;
+	else
+		cout << "boii if you don't get out of here." << endl;
+
+
+}
